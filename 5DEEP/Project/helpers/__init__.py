c3c3da489e5bc88a30e67744946d264ba89e4bf5
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2023-02-15 20:08:19 +0100

    Moved functions to the helpers module

--- 5DEEP/Project/helpers/__init__.py
+++ 5DEEP/Project/helpers/__init__.py
@@ -3,8 +3,9 @@
 #           Utility functions used in the notebooks.                                                                   #
 # #################################################################################################################### #
 
-__all__ = ["ckpts", "perfs", "urban_df"]
+__all__ = ["audio", "ckpts", "perfs", "urban_df"]
 
+from . import audio
 from . import ckpts
 from . import perfs
 from . import urban_df
